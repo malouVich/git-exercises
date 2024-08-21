e4ae00dd6249d2ac1a540bdd2874cd01c43f1fdc
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-08-21 15:28:33 +0200

    countdownChange

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int i = 0 + 1; i <= 100; i++) {
+        for (int i = 0; i <= 100; i++) {
 
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
